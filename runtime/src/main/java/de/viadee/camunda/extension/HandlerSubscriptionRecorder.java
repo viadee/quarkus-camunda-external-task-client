efaf056091697508bf872a270ce77de5a602c172
Christoph Schoennenbeck <christoph.schoennenbeck@viadee.de>
2022-01-17 08:49:26 +0100

    fix subscription recording
    Signed-off-by: Christoph Schoennenbeck <christoph.schoennenbeck@viadee.de>

--- runtime/src/main/java/de/viadee/camunda/extension/HandlerSubscriptionRecorder.java
+++ runtime/src/main/java/de/viadee/camunda/extension/HandlerSubscriptionRecorder.java
@@ -1,5 +1,6 @@
 package de.viadee.camunda.extension;
 
+import io.quarkus.arc.Arc;
 import io.quarkus.runtime.annotations.Recorder;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
@@ -15,24 +16,25 @@ public class HandlerSubscriptionRecorder {
     ExternalTaskClient externalTaskClient;
 
     private void registerHandler(ExternalTaskHandler externalTaskHandler, String topic) {
-        externalTaskClient.subscribe(topic).handler(externalTaskHandler).open();
+        externalTaskClient.subscribe(topic)
+                .handler(externalTaskHandler).open();
     }
 
 
     public void registerHandlers() {
-        CDI.current().select(ExternalTaskHandler.class).stream().forEach(
-                handler -> {
-                    var annotation = Arrays.stream(handler.getClass().getAnnotations())
-                            .filter(a -> a instanceof ExternalTaskSubscription)
-                            .findFirst();
-                    if (annotation.isEmpty()) {
-                        return;
-                    }
-                    var castAnnotation =  (ExternalTaskSubscription) annotation.get();
+        Arc.container().select(ExternalTaskHandler.class).handles().forEach(instanceHandle -> {
+            var annotation = Arrays.stream(instanceHandle.getBean().getBeanClass().getAnnotations())
+                    .filter(a -> a instanceof ExternalTaskSubscription)
+                    .findFirst();
+            if (annotation.isEmpty()) {
+                return;
+            }
+            var castAnnotation =  (ExternalTaskSubscription) annotation.get();
 
-                    this.registerHandler(handler,castAnnotation.topicName());
-                }
-        );
+            this.registerHandler(instanceHandle.get(),castAnnotation.topicName());
+
+        });
+
     }
 
     // todo: open at runttime, subscription building at buildtime?
